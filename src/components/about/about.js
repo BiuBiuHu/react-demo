d172cb32fcac854505939ef5dc116ad9151bd45a
= <yueshenghu@ascs.tech>
2018-10-07 17:20:32 +0800

    feat: first commit

---
+++ src/components/about/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../common/NavBar';
+const About = () => (
+    <div>
+        <NavBar title="关于页" />
+        <h1>这是关于页</h1>
+    </div>
+);
+
+export default About;
---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../common/NavBar';
+
+const Home = () => (
+    <div>
+        <NavBar title="首页" />
+        <h1>这是首页</h1>
+    </div>
+);
+
+export default Home;

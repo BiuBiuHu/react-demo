603078d09ed471ffdaa81f80172b3dcd71d9a23b
= <yueshenghu@ascs.tech>
2018-10-07 17:47:53 +0800

    feat: 增加ajax请求方式，增加公共组件，以及组件继承

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {
     Link,
 } from 'react-router-dom';
-
+import './NavBar.css';
 const NavBar = (props) => (
-    <ul>
+    <ul className="nav-bar">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/list">List</Link></li>
